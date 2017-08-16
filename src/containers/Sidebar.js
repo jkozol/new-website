deea78c44961768d86f416c819c45e4009bd18c6
Jacob Kozol <jacobdkozol@gmail.com>
2017-08-16 17:28:41 -0400

    made a sidebar

---
+++ src/containers/Sidebar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+class Sidebar extends React.Component {
+  state = {
+    color: 'black',
+  }
+
+  componentWillMount() {
+    this.setColor(this.props.links[0].color);
+  }
+
+  setColor = (color) => {
+    this.setState({color: color});
+  }
+
+  render() {
+    const { links } = this.props;
+    const navStyle = {outlineColor: this.state.color};
+    const linkStyle = {color: this.state.color};
+    return (
+      <div className="row">
+        <div className="col-md-2 col-xl-1 ml-5">
+          <div className="sideNav" style={navStyle}>
+            <ul className="nav flex-column text-center">
+              {links.map((link) =>
+                <li className="nav-item" key={link.name}>
+                  <NavLink className="nav-link" to={link.path} onMouseOver={() => this.setColor(link.color)}  onMouseLeave={() => this.setColor('black')}  onClick={() => this.setColor(link.color)} activeStyle={linkStyle}>{link.name}</NavLink>
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Sidebar;
