7d14d56e8afa026b9c59997385299f3d6e5a74df
Jacob Kozol <jacobdkozol@gmail.com>
2017-08-17 16:55:17 -0400

    starting to get layout

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
-import { configureStore } from './configureStore';
+import { configureStore } from './modules/store';
 import App from './containers/App';
 
 const store = configureStore();
--- src/modules/store.js
+++ src/modules/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
-import rootReducer from './modules';
+import rootReducer from './reducers/index';
 
 export const history = createHistory();
 
